c917633d7f8becdb65d97a5e92ccb67150d42b6b
KyleTryon <git@techsquidtv.com>
2023-01-06 14:32:12 -0500

    fix: fixup

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-
 # This script is used to run the application in a docker container.
 echo "Building Docker image..."
 docker build -t cci-env-inspector .
 echo "Running Docker image..."
-docker run --name my-container -it cci-env-inspector 
-docker cp my-container:circleci-data.json
+docker run --name my-container -it cci-env-inspector
+docker cp my-container:/project/circleci-data.json circleci-data.json
 docker container rm my-container
