f221501333036babeca45970bb1b620fdf63423a
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-01-10 09:27:45 -0800

    chore: add eslint (#27)
    * chore: add eslint
    
    * chore: include lint in package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: [ "./tsconfig.json" ]
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-tsdoc',
+    'security',
+    'prettier',
+  ],
+  rules: {
+    'tsdoc/syntax': 'warn',
+    '@typescript-eslint/indent': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': 'error',
+    'security/detect-object-injection': 'off',
+  }
+};
