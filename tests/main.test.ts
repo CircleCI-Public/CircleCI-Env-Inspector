027538c2ea649c6a57bbba99cb4e72b127fe367a
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-01-24 12:53:11 -0800

    feat: introduce ts-jest and test example (#48)

---
+++ tests/main.test.ts
@@ -0,0 +1,6 @@
+// Placeholder test to ensure tests are running
+describe("Ensure tests are running", () => {
+  it("should return success", () => {
+    expect("Hello There!").toBe("Hello There!");
+  });
+});
