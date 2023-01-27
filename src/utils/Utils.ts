af6e6a1a067f44f60de10664d23a601caf3bd349
KyleTryon <git@techsquidtv.com>
2023-01-26 22:32:35 -0500

    feat: printError function

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,10 +1,9 @@
 import chalk from "chalk";
 
-export function exitOnError(e: Error, title?: string) {
-  console.error(
-    chalk.bold.red(title ?? "Fatal error: "),
-    chalk.bold(e.message ?? e ?? "Unknown error")
-  );
+export function exitOnError(e: Error, title?: string, indent?: number) {
+  const titleStyled = title ? chalk.red(title) : "";
+  const indentStyled = indent ? " ".repeat(indent) : "";
+  console.error(chalk.bold(`${indentStyled}${titleStyled} ${e.message}`));
   process.exit(1);
 }
 
@@ -12,4 +11,10 @@ export function printMessage(message: string, title?: string, indent?: number) {
   const titleStyled = title ? chalk.green(title) : "";
   const indentStyled = indent ? " ".repeat(indent) : "";
   console.log(chalk.bold(`${indentStyled}${titleStyled} ${message}`));
+}
+
+export function printError(message: string, title?: string, indent?: number) {
+  const titleStyled = title ? chalk.red(title) : "";
+  const indentStyled = indent ? " ".repeat(indent) : "";
+  console.error(chalk.bold(`${indentStyled}${titleStyled} ${message}`));
 }
