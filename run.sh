a335afb82a6ed7b31379c270cb31883989f1fdc7
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-02-02 09:21:39 -0800

    fix: remove brackets from conditional predicate (#63)

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ which podman &>/dev/null ]; then
+if which podman &>/dev/null; then
     OCI_IMAGE_RUNNER="podman"
-elif [ which docker &>/dev/null ]; then
+elif which docker &>/dev/null; then
     OCI_IMAGE_RUNNER="docker"
 else
     echo "Please install podman or docker. You can also run via Node with 'npm start'."
@@ -10,11 +10,11 @@ else
 fi
 # This script is used to run the application in a docker container.
 echo "Building Docker image..."
-${OCI_IMAGE_RUNNER} build -t cci-env-inspector .
+"${OCI_IMAGE_RUNNER}" build -t cci-env-inspector .
 echo "Running Docker image..."
-${OCI_IMAGE_RUNNER} run --name circleci-env-inspector -it cci-env-inspector
+"${OCI_IMAGE_RUNNER}" run --name circleci-env-inspector -it cci-env-inspector
 echo "Extracting data from container..."
-${OCI_IMAGE_RUNNER} cp circleci-env-inspector:/project/circleci-data.json circleci-data.json
+"${OCI_IMAGE_RUNNER}" cp circleci-env-inspector:/project/circleci-data.json circleci-data.json
 echo "Cleaning up..."
-${OCI_IMAGE_RUNNER} container rm circleci-env-inspector &>/dev/null
+"${OCI_IMAGE_RUNNER}" container rm circleci-env-inspector &>/dev/null
 echo "Done."
