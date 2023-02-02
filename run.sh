c21a567750d0c624411114c10b19a84a4007b134
Kyle a.k.a. TechSquidTV <33272306+KyleTryon@users.noreply.github.com>
2023-02-02 11:47:58 -0500

    feat: podman and docker support in run (#61)

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+if [ which podman &>/dev/null ]; then
+    OCI_IMAGE_RUNNER="podman"
+elif [ which docker &>/dev/null ]; then
+    OCI_IMAGE_RUNNER="docker"
+else
+    echo "Please install podman or docker. You can also run via Node with 'npm start'."
+    exit 1
+fi
 # This script is used to run the application in a docker container.
 echo "Building Docker image..."
-docker build -t cci-env-inspector .
+${OCI_IMAGE_RUNNER} build -t cci-env-inspector .
 echo "Running Docker image..."
-docker run --name circleci-env-inspector -it cci-env-inspector
+${OCI_IMAGE_RUNNER} run --name circleci-env-inspector -it cci-env-inspector
 echo "Extracting data from container..."
-docker cp circleci-env-inspector:/project/circleci-data.json circleci-data.json
+${OCI_IMAGE_RUNNER} cp circleci-env-inspector:/project/circleci-data.json circleci-data.json
 echo "Cleaning up..."
-docker container rm circleci-env-inspector &>/dev/null
+${OCI_IMAGE_RUNNER} container rm circleci-env-inspector &>/dev/null
 echo "Done."
